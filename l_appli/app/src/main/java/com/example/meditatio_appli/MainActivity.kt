988acd6cdd38b0b7b8da99a532f5239f6f266bfd
Maxim Bocquillion <maxim.bocquillion@epita.fr>
2023-02-13 17:39:57 -0500

    on reessaye hein

---
+++ l_appli/app/src/main/java/com/example/meditatio_appli/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.example.meditatio_appli
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.meditatio_appli.ui.theme.Meditatio_AppliTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Meditatio_AppliTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    Meditatio_AppliTheme {
+        Greeting("Android")
+    }
+}
