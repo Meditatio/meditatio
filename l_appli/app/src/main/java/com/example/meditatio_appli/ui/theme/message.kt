87ccedc3f55fc2706e221c481e6b76cecf3bb6e2
Maxim Bocquillion <maxim.bocquillion@epita.fr>
2023-02-16 17:57:52 -0500

    creating files for message

---
+++ l_appli/app/src/main/java/com/example/meditatio_appli/ui/theme/message.kt
@@ -0,0 +1,12 @@
+package com.example.meditatio_appli.ui.theme
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.meditatio_appli.R
+
+class message : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_message)
+    }
+}
