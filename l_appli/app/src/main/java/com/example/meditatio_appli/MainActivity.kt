fdf7dc1a090632d10e223c27d7e4ed56ecd69a6c
Mathieu Even <mathieu.even@epita.fr>
2023-02-15 22:51:45 -0500

    Ajout activité vidéos youtube

--- l_appli/app/src/main/java/com/example/meditatio_appli/MainActivity.kt
+++ l_appli/app/src/main/java/com/example/meditatio_appli/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.meditatio_appli
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Info
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.meditatio_appli.ui.theme.Meditatio_AppliTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +33,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainContent()
                 }
             }
         }
@@ -30,14 +41,65 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ValidateButton() { // function to show the validate button
+    val context = LocalContext.current
+
+    Button(
+        shape = RoundedCornerShape(30.dp),
+        modifier = Modifier.fillMaxWidth(),
+        onClick = {
+            loadSecondPage(context)
+        }) {
+        Icon(
+            imageVector = Icons.Default.Info,
+            contentDescription = "Youtube Page"
+        )
+        Text(text = "Youtube Page")
+    }
 }
+
+fun loadSecondPage(context: Context)
+{
+    Toast.makeText(context, "You go at the Youtube page", Toast.LENGTH_LONG).show()
+    val intent = Intent(context, Youtube::class.java)
+    context.startActivity(intent)
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun MainContent() { // function to show wrap all the previous functions
+    val scaffoldState = rememberScaffoldState(rememberDrawerState(DrawerValue.Closed))
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        scaffoldState = scaffoldState,
+        topBar = { TopAppBar(title = {Text("Meditatio")},backgroundColor = MaterialTheme.colors.primary)  },
+
+        drawerContent = { Text(text = "Drawer Menu 1") },
+
+        content = {
+            Column(
+                horizontalAlignment = Alignment.CenterHorizontally,
+                modifier = Modifier
+                    .padding(20.dp)
+                    .verticalScroll(rememberScrollState()))
+            {
+
+
+                Spacer(modifier = Modifier.height(50.dp))
+                ValidateButton()
+            }
+        },
+        //bottomBar = { BottomAppBar(backgroundColor = MaterialTheme.colors.primary) { Text("Bottom Bar") } }
+    )
+}
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+
     Meditatio_AppliTheme {
-        Greeting("Android")
+        MainContent()
     }
 }
--- l_appli/app/src/main/java/com/example/meditatio_appli/Youtube.kt
+++ l_appli/app/src/main/java/com/example/meditatio_appli/Youtube.kt
@@ -1,11 +1,66 @@
 package com.example.meditatio_appli
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
-class Youtube : AppCompatActivity() {
+class Youtube : YouTubeBaseActivity() {
+
+
+    val VIDEO_ID = "vduKkK10eag"
+    val YOUTUBE_API_KEY = "AIzaSyA-gcsPXsUsTFq2ByPxdKUlI0IBd7aNQi8"
+
+    private lateinit var youTubePlayer: YouTubePlayerView
+    private lateinit var btnPlay : Button
+    private lateinit var btnReturn : Button
+
+    lateinit var youtubePlayerInit : YouTubePlayer.OnInitializedListener
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_youtube)
+
+        youTubePlayer = findViewById(R.id.youtubePlayer)
+        btnPlay = findViewById(R.id.btnPlay)
+        btnReturn = findViewById(R.id.btnReturn)
+
+        youtubePlayerInit = object : YouTubePlayer.OnInitializedListener {
+            override fun onInitializationSuccess(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubePlayer?,
+                p2: Boolean
+            ) {
+                p1?.loadVideo(VIDEO_ID)
+            }
+
+            override fun onInitializationFailure(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubeInitializationResult?
+            ) {
+                Toast.makeText(applicationContext,"Failed",Toast.LENGTH_SHORT).show()
+            }
+        }
+        btnPlay.setOnClickListener{
+            youTubePlayer.initialize(YOUTUBE_API_KEY,youtubePlayerInit)
+        }
+        btnReturn.setOnClickListener{
+            loadFirstPage(applicationContext)
+        }
+    }
+    fun loadFirstPage(context: Context)
+    {
+        Toast.makeText(context, "You return at the first page", Toast.LENGTH_LONG).show()
+        val intent = Intent(context, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK;
+        context.startActivity(intent)
     }
 }
