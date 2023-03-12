8dd6daf74e162664757f883cf84f37e32c703f6b
Maxim Bocquillion <maxim.bocquillion@epita.fr>
2023-03-12 13:22:56 -0400

    video 4 - 10-34

--- l_appli/app/src/main/java/com/example/meditatio_appli/LatestMessagesActivity.kt
+++ l_appli/app/src/main/java/com/example/meditatio_appli/LatestMessagesActivity.kt
@@ -1,11 +1,32 @@
 package com.example.meditatio_appli
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import com.google.firebase.auth.FirebaseAuth
 
 class LatestMessagesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_latest_messages)
+
+        verifyUserIsLoggedIn()
+
+    }
+
+    private fun verifyUserIsLoggedIn()
+    {
+        val uid = FirebaseAuth.getInstance().uid
+        if (uid == null)
+        {
+            val intent = Intent(this, RegisterActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+            startActivity(intent)
+        }
+    }
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.nav_menu, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
