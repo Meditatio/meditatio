f3afb8e7e80c0a23bc169a008f68b6f5b6f6c0ab
Maxim Bocquillion <maxim.bocquillion@epita.fr>
2023-03-12 17:51:20 -0400

    video 4 - 24:38

--- l_appli/app/src/main/java/com/example/meditatio_appli/NewMessageActivity.kt
+++ l_appli/app/src/main/java/com/example/meditatio_appli/NewMessageActivity.kt
@@ -2,10 +2,51 @@ package com.example.meditatio_appli
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.*
+import com.xwray.groupie.GroupAdapter
+import com.xwray.groupie.GroupieViewHolder
+import com.xwray.groupie.Item
 
 class NewMessageActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new_message)
+
+        supportActionBar?.title = "Select User"
+        val recyclerview_newmessage = findViewById<RecyclerView>(R.id.recyclerview_newmessage)
+
+        val adapter = GroupAdapter<GroupieViewHolder>()
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+        adapter.add(UserItem())
+
+
+
+        recyclerview_newmessage.adapter = adapter
     }
 }
+
+class UserItem: Item<GroupieViewHolder>()
+{
+    override fun bind(p0: GroupieViewHolder, p1: Int)
+    {
+        //will be called in our list for each user object later on...
+    }
+
+    override fun getLayout():Int
+    {
+        return R.layout.user_row_newmessage
+    }
+}
+
+/*
+class CustomAdapter:RecyclerView.Adapter<ViewHolder>
+{
+    override fun OnBindViewHolder(p0:, p1:Int)
+    {
+
+    }
+}*/
