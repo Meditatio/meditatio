f7780d8d011fb9da6d0a599c416f7f0d43330fc8
Maxim Bocquillion <maxim.bocquillion@epita.fr>
2023-03-26 19:45:51 -0400

    video 8 8:04 - tjrs push quand ça marche

--- l_appli/app/src/main/java/messages/LatestMessagesActivity.kt
+++ l_appli/app/src/main/java/messages/LatestMessagesActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.meditatio_appli.R
@@ -17,6 +18,7 @@ import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.squareup.picasso.Picasso
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.GroupieViewHolder
 import com.xwray.groupie.Item
@@ -49,6 +51,36 @@ class LatestMessagesActivity : AppCompatActivity() {
         override fun bind(p0: GroupieViewHolder, p1: Int) {
             val message_textview_latest_message = p0.itemView.findViewById<TextView>(R.id.message_textview_latest_message)
             message_textview_latest_message.text = chatMessage.text
+
+            val username_textview_latest_message = p0.itemView.findViewById<TextView>(R.id.username_textview_latest_message)
+
+
+            val chatPartnerId:String
+            if (chatMessage.fromId == FirebaseAuth.getInstance().uid)
+            {
+                chatPartnerId = chatMessage.toId
+            }
+            else
+            {
+                chatPartnerId = chatMessage.fromId
+            }
+            val ref = FirebaseDatabase.getInstance().getReference("/users/$chatPartnerId")
+            ref.addListenerForSingleValueEvent(object: ValueEventListener
+            {
+                override fun onDataChange(p2: DataSnapshot)
+                {
+                    val user = p2.getValue(User::class.java)
+                    username_textview_latest_message.text = user?.username
+
+                    val targetImageView = p0.itemView.findViewById<ImageView>(R.id.imageview_latest_message)
+                    Picasso.get().load(user?.profileImageUrl).into(targetImageView)
+                }
+                override fun onCancelled(p2: DatabaseError)
+                {
+
+                }
+            })
+
         }
         override fun getLayout(): Int {
             return R.layout.latest_message_row
