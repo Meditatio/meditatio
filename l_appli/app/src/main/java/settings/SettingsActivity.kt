1333bd7445a43a3042deccb05be6cc6e8c0567f0
Hadarios <nolan.djellal@gmail.com>
2023-03-27 21:05:00 -0400

    boutton sign out dans paramètres

--- l_appli/app/src/main/java/settings/SettingsActivity.kt
+++ l_appli/app/src/main/java/settings/SettingsActivity.kt
@@ -1,11 +1,15 @@
 package settings
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.NavUtils
 import androidx.preference.PreferenceFragmentCompat
 import com.example.meditatio_appli.R
+import com.google.firebase.auth.FirebaseAuth
+import registerlogin.RegisterActivity
 
 
 class SettingsActivity : AppCompatActivity() {
@@ -31,10 +35,22 @@ class SettingsActivity : AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.settings_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId) {
             android.R.id.home -> {
                 NavUtils.navigateUpFromSameTask(this)
+                true
+            }
+            R.id.item1 -> {
+                FirebaseAuth.getInstance().signOut()
+                val intent = Intent(this, RegisterActivity::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK.or(Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
                 true
             }
             else -> super.onOptionsItemSelected(item)
